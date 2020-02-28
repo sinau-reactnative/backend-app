599eb7360ccbb31337b6e37963a0129fddfd8801
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2020-02-28 20:38:15 +0700

    added pagination users

--- controllers/users.js
+++ controllers/users.js
@@ -1,46 +1,70 @@
-const db = require("../configs/db");
-const { sendResponse } = require("../helpers/response");
+const db = require("../configs/db")
+const { sendResponse } = require("../helpers/response")
 
 module.exports = {
   getAllUser: (req, res) => {
-    const { role } = req.query;
-    let sql = `SELECT * FROM users `;
-    role ? (sql += `WHERE role = ?`) : null;
-    sql += `ORDER BY updated_at DESC LIMIT 10 OFFSET 0`;
+    const { limit, offset, role } = req.query
+    let sql = `SELECT * FROM users `
+    role ? (sql += `WHERE role = ?`) : null
+    sql += `ORDER BY updated_at DESC LIMIT ${Number(limit) ||
+      20} OFFSET ${Number(offset) || 0}`
 
-    try {
+    const total = `SELECT COUNT(id) as total FROM users`
+
+    const getSql = new Promise((resolve, reject) => {
       db.query(sql, role ? [role] : [], (err, result) => {
-        if (err) {
-          sendResponse(res, 500, err);
-        } else {
-          sendResponse(res, 200, result);
+        if (err) reject(err)
+        else resolve(result)
+      })
+    })
+
+    const totalSql = new Promise((resolve, reject) => {
+      db.query(total, [], (err, result) => {
+        if (err) reject(err)
+        else resolve(result)
+      })
+    })
+
+    Promise.all([getSql, totalSql])
+      .then(result => {
+        const page = offset / limit + 1
+        const total = result[1].map(i => i.total)[0]
+        const hasNext = total - page * limit > 0 ? true : false
+        const pagination = {
+          page,
+          hasNext,
+          total
         }
-      });
-    } catch (error) {
-      sendResponse(res, 500, error);
-    }
+        sendResponse(res, 200, { result: result[0], pagination })
+      })
+      .catch(err => {
+        sendResponse(res, 500, {
+          response: "error_when_get_all_users",
+          err
+        })
+      })
   },
 
   getUserById: (req, res) => {
-    const { id } = req.params;
-    const sql = `SELECT * FROM users WHERE id= ?;`;
+    const { id } = req.params
+    const sql = `SELECT * FROM users WHERE id= ?;`
 
     try {
       db.query(sql, [id], (err, result) => {
         if (err) {
-          sendResponse(res, 500, err);
+          sendResponse(res, 500, err)
         } else {
-          sendResponse(res, 200, result);
+          sendResponse(res, 200, result)
         }
-      });
+      })
     } catch (error) {
-      sendResponse(res, 500, error);
+      sendResponse(res, 500, error)
     }
   },
 
   updateUser: (req, res) => {
-    const { id } = req.params;
-    const { fullname, email, username, address, role } = req.body;
+    const { id } = req.params
+    const { fullname, email, username, address, role } = req.body
     const sql = `
         UPDATE users
         SET fullname = ?,
@@ -49,7 +73,7 @@ module.exports = {
             address = ?,
             role = ?
         WHERE id = ?
-    ;`;
+    ;`
 
     try {
       db.query(
@@ -57,33 +81,33 @@ module.exports = {
         [fullname, email, username, address, role, id],
         (err, result) => {
           if (err) {
-            sendResponse(res, 500, err);
+            sendResponse(res, 500, err)
           } else {
             sendResponse(res, 200, {
               msg: `data has been updated => ${result}`
-            });
+            })
           }
         }
-      );
+      )
     } catch (error) {
-      sendResponse(res, 500, error);
+      sendResponse(res, 500, error)
     }
   },
 
   deleteUserById: (req, res) => {
-    const { id } = req.params;
-    const sql = `DELETE FROM users WHERE id = ?`;
+    const { id } = req.params
+    const sql = `DELETE FROM users WHERE id = ?`
 
     try {
       db.query(sql, [id], (err, result) => {
         if (err) {
-          sendResponse(res, 500, err);
+          sendResponse(res, 500, err)
         } else {
-          sendResponse(res, 200, result);
+          sendResponse(res, 200, result)
         }
-      });
+      })
     } catch (error) {
-      sendResponse(res, 500, error);
+      sendResponse(res, 500, error)
     }
   }
-};
+}
