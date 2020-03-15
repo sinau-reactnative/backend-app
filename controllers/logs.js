21f0a6ee7204f8cc36daaf63aee3b3d28e4162c6
ayogatot <azerino25@gmail.com>
2020-03-15 21:59:36 +0700

    add pagination for logs

--- controllers/logs.js
+++ controllers/logs.js
@@ -3,6 +3,7 @@ const { sendResponse } = require("../helpers/response");
 
 module.exports = {
   getTenantLogs: (req, res) => {
+    const { offset, limit } = req.query;
     const { id } = req.params;
 
     const sql = `
@@ -14,17 +15,44 @@ module.exports = {
         ORDER BY L.created_at DESC;
         ;
     `;
+    let total = `SELECT COUNT(*) as total FROM tenant_logs WHERE tenant_id = ? `;
 
-    db.query(sql, [id], (err, result) => {
-      if (err) {
-        sendResponse(res, 500, { response: "error_when_get_tenant_logs", err });
-      } else {
-        sendResponse(res, 200, result);
-      }
+    const getSql = new Promise((resolve, reject) => {
+      db.query(sql, [id], (err, result) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
     });
+
+    const totalSql = new Promise((resolve, reject) => {
+      db.query(total, [id], (err, result) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+
+    Promise.all([getSql, totalSql])
+      .then(result => {
+        const page = offset / limit + 1;
+        const total = result[1].map(i => i.total)[0];
+        const hasNext = total - page * limit > 0 ? true : false;
+        const pagination = {
+          page,
+          hasNext,
+          total
+        };
+        sendResponse(res, 200, { result: result[0], pagination });
+      })
+      .catch(err => {
+        sendResponse(res, 500, {
+          response: "error_when_get_all_tenant_logs",
+          err
+        });
+      });
   },
 
   getMerchantLogs: (req, res) => {
+    const { offset, limit } = req.query;
     const { id } = req.params;
 
     const sql = `
@@ -36,16 +64,44 @@ module.exports = {
         ORDER BY L.created_at DESC;
     `;
 
-    db.query(sql, [id], (err, result) => {
-      if (err) {
-        sendResponse(res, 500, { response: "error_when_get_merchant_logs", err });
-      } else {
-        sendResponse(res, 200, result);
-      }
+    let total = `SELECT COUNT(*) as total FROM merchant_logs WHERE merchant_id = ? `;
+
+    const getSql = new Promise((resolve, reject) => {
+      db.query(sql, [id], (err, result) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
     });
+
+    const totalSql = new Promise((resolve, reject) => {
+      db.query(total, [id], (err, result) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+
+    Promise.all([getSql, totalSql])
+      .then(result => {
+        const page = offset / limit + 1;
+        const total = result[1].map(i => i.total)[0];
+        const hasNext = total - page * limit > 0 ? true : false;
+        const pagination = {
+          page,
+          hasNext,
+          total
+        };
+        sendResponse(res, 200, { result: result[0], pagination });
+      })
+      .catch(err => {
+        sendResponse(res, 500, {
+          response: "error_when_get_all_merchant_logs",
+          err
+        });
+      });
   },
 
   getBillingLogs: (req, res) => {
+    const { offset, limit } = req.query;
     const { id } = req.params;
 
     const sql = `
@@ -57,12 +113,39 @@ module.exports = {
         ORDER BY L.created_at DESC;
     `;
 
-    db.query(sql, [id], (err, result) => {
-      if (err) {
-        sendResponse(res, 500, { response: "error_when_get_merchant_logs", err });
-      } else {
-        sendResponse(res, 200, result);
-      }
+    let total = `SELECT COUNT(*) as total FROM billing_logs WHERE id = ? `;
+
+    const getSql = new Promise((resolve, reject) => {
+      db.query(sql, [id], (err, result) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
     });
+
+    const totalSql = new Promise((resolve, reject) => {
+      db.query(total, [id], (err, result) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+
+    Promise.all([getSql, totalSql])
+      .then(result => {
+        const page = offset / limit + 1;
+        const total = result[1].map(i => i.total)[0];
+        const hasNext = total - page * limit > 0 ? true : false;
+        const pagination = {
+          page,
+          hasNext,
+          total
+        };
+        sendResponse(res, 200, { result: result[0], pagination });
+      })
+      .catch(err => {
+        sendResponse(res, 500, {
+          response: "error_when_get_all_billing_logs",
+          err
+        });
+      });
   }
 };
