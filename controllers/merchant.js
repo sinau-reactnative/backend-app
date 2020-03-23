425efd0a790650c2bd1e7c8e13877bbe96ee0c88
Arsyad Sukma Ramadhan <blacklistdark1987@gmail.com>
2020-03-24 00:08:40 +0700

    add total merchant

--- controllers/merchant.js
+++ controllers/merchant.js
@@ -1,11 +1,11 @@
-const db = require("../configs/db");
-const stringify = require("csv-stringify");
-const AWS_LINK = process.env.AWS_LINK;
+const db = require("../configs/db")
+const stringify = require("csv-stringify")
+const AWS_LINK = process.env.AWS_LINK
 
-const { sendResponse } = require("../helpers/response");
-const { uploadFile } = require("../helpers/upload");
-const { merchantLogs } = require("../helpers/updateLogs");
-const { exportToExcel } = require("../helpers/export");
+const { sendResponse } = require("../helpers/response")
+const { uploadFile } = require("../helpers/upload")
+const { merchantLogs } = require("../helpers/updateLogs")
+const { exportToExcel } = require("../helpers/export")
 
 module.exports = {
   createMerchant: (req, res) => {
@@ -19,14 +19,14 @@ module.exports = {
       merchant_space,
       price_per_meter,
       total_price
-    } = req.body;
-    const user_id = req.user[0].id;
+    } = req.body
+    const user_id = req.user[0].id
 
-    let _tenantId = merchant_status === "bebas" ? "0000000000000000" : tenant_id;
-    let attachment_1 = req.files["attachment_1"];
-    let attachment_2 = req.files["attachment_2"];
-    let _attachment_1;
-    let _attachment_2;
+    let _tenantId = merchant_status === "bebas" ? "0000000000000000" : tenant_id
+    let attachment_1 = req.files["attachment_1"]
+    let attachment_2 = req.files["attachment_2"]
+    let _attachment_1
+    let _attachment_2
     let _data = [
       merchant_no,
       _tenantId,
@@ -37,30 +37,30 @@ module.exports = {
       merchant_space,
       price_per_meter,
       total_price
-    ];
+    ]
 
     if (attachment_1 && attachment_2) {
-      uploadFile(attachment_1[0], "attachment_1", merchant_no);
-      uploadFile(attachment_2[0], "attachment_2", merchant_no);
+      uploadFile(attachment_1[0], "attachment_1", merchant_no)
+      uploadFile(attachment_2[0], "attachment_2", merchant_no)
 
-      _attachment_1 = `${AWS_LINK}${merchant_no}-attachment_1.jpg`;
-      _attachment_2 = `${AWS_LINK}${merchant_no}-attachment_2.jpg`;
+      _attachment_1 = `${AWS_LINK}${merchant_no}-attachment_1.jpg`
+      _attachment_2 = `${AWS_LINK}${merchant_no}-attachment_2.jpg`
 
-      _data.push(_attachment_1);
-      _data.push(_attachment_2);
+      _data.push(_attachment_1)
+      _data.push(_attachment_2)
     } else if (attachment_1) {
-      uploadFile(attachment_1[0], "attachment_1", merchant_no);
-      _attachment_1 = `${AWS_LINK}${merchant_no}-attachment_1.jpg`;
-      _data.push(_attachment_1);
-      _data.push("");
+      uploadFile(attachment_1[0], "attachment_1", merchant_no)
+      _attachment_1 = `${AWS_LINK}${merchant_no}-attachment_1.jpg`
+      _data.push(_attachment_1)
+      _data.push("")
     } else if (attachment_2) {
-      uploadFile(attachment_2[0], "attachment_2", merchant_no);
-      _attachment_2 = `${AWS_LINK}${merchant_no}-attachment_2.jpg`;
-      _data.push(_attachment_2);
-      _data.push("");
+      uploadFile(attachment_2[0], "attachment_2", merchant_no)
+      _attachment_2 = `${AWS_LINK}${merchant_no}-attachment_2.jpg`
+      _data.push(_attachment_2)
+      _data.push("")
     } else {
-      _data.push("");
-      _data.push("");
+      _data.push("")
+      _data.push("")
     }
 
     const sql = `
@@ -80,7 +80,7 @@ module.exports = {
             DEFAULT,
             DEFAULT
         );
-    `;
+    `
 
     const logSql = `
         INSERT INTO merchant_logs
@@ -93,33 +93,35 @@ module.exports = {
             DEFAULT,
             DEFAULT
         );
-    `;
+    `
 
     db.query(sql, _data, err => {
       if (err) {
         sendResponse(res, 500, {
           response: "error_when_create_new_merchant",
           err
-        });
+        })
       } else {
         db.query(logSql, [
           user_id,
           merchant_no,
           "-",
           `Merchant dengan nomer = ${merchant_no} berhasil dibuat`
-        ]);
-        sendResponse(res, 200, { id: merchant_no });
+        ])
+        sendResponse(res, 200, { id: merchant_no })
       }
-    });
+    })
   },
 
   getAllMerchants: (req, res) => {
-    const { limit, offset, search, type, csv, xls, filter } = req.query;
-    let total = `SELECT COUNT(*) as total FROM merchants M `;
+    const { limit, offset, search, type, csv, xls, filter } = req.query
+    let total = `SELECT COUNT(*) as total FROM merchants M `
+    let totalEksisting = `SELECT COUNT(*) as total FROM merchants M WHERE M.merchant_status = 'eksisting' AND EXISTS (SELECT * FROM billings B WHERE B.payment_term = 'booking_fee' AND B.merchant_id = M.merchant_no)`
+    let totalBebas = `SELECT COUNT(*) as total FROM merchants M WHERE M.merchant_status = 'bebas'`
 
     // Download CSV ?
-    let _csv = csv ? (csv === "true" ? true : false) : false;
-    let _xls = xls ? (xls === "true" ? true : false) : false;
+    let _csv = csv ? (csv === "true" ? true : false) : false
+    let _xls = xls ? (xls === "true" ? true : false) : false
 
     let sql = `
     SELECT *, T.name,
@@ -130,74 +132,98 @@ module.exports = {
            FROM merchants M
            JOIN tenants T
            ON M.tenant_id = T.no_ktp
-    `;
+    `
 
     if (search && type === "name") {
-      sql += `WHERE T.name LIKE '%${search}%'`;
-      total += `JOIN tenants T ON M.tenant_id = T.no_ktp WHERE T.name LIKE '%${search}%'`;
+      sql += `WHERE T.name LIKE '%${search}%'`
+      total += `JOIN tenants T ON M.tenant_id = T.no_ktp WHERE T.name LIKE '%${search}%'`
     } else if (search && type === "nik") {
-      sql += `WHERE T.no_ktp LIKE '%${search}%'`;
-      total += `JOIN tenants T ON M.tenant_id = T.no_ktp WHERE T.no_ktp LIKE '%${search}%'`;
+      sql += `WHERE T.no_ktp LIKE '%${search}%'`
+      total += `JOIN tenants T ON M.tenant_id = T.no_ktp WHERE T.no_ktp LIKE '%${search}%'`
     } else if (search && type === "merchant_no") {
-      sql += `WHERE M.merchant_no LIKE '%${search}%'`;
-      total += `JOIN tenants T ON M.tenant_id = T.no_ktp WHERE M.merchant_no LIKE '%${search}%'`;
+      sql += `WHERE M.merchant_no LIKE '%${search}%'`
+      total += `JOIN tenants T ON M.tenant_id = T.no_ktp WHERE M.merchant_no LIKE '%${search}%'`
     } else if (filter === "eksisting") {
-      sql += `WHERE M.merchant_status = 'eksisting' AND EXISTS (SELECT * FROM billings B WHERE B.payment_term = 'booking_fee' AND B.merchant_id = M.merchant_no) `;
-      total += `JOIN tenants T ON M.tenant_id = T.no_ktp WHERE M.merchant_status = 'eksisting'`;
+      sql += `WHERE M.merchant_status = 'eksisting' AND EXISTS (SELECT * FROM billings B WHERE B.payment_term = 'booking_fee' AND B.merchant_id = M.merchant_no) `
+      total += `JOIN tenants T ON M.tenant_id = T.no_ktp WHERE M.merchant_status = 'eksisting'`
     } else if (filter === "bebas") {
-      sql += `WHERE M.merchant_status = 'bebas'`;
-      total += `JOIN tenants T ON M.tenant_id = T.no_ktp WHERE M.merchant_status = 'bebas'`;
+      sql += `WHERE M.merchant_status = 'bebas'`
+      total += `JOIN tenants T ON M.tenant_id = T.no_ktp WHERE M.merchant_status = 'bebas'`
     }
 
-    sql += `LIMIT ${Number(limit) || 20} OFFSET ${Number(offset) || 0};`;
+    sql += `LIMIT ${Number(limit) || 20} OFFSET ${Number(offset) || 0};`
     const getSql = new Promise((resolve, reject) => {
       db.query(sql, [], (err, result) => {
-        if (err) reject(err);
-        else resolve(result);
-      });
-    });
+        if (err) reject(err)
+        else resolve(result)
+      })
+    })
 
     const totalSql = new Promise((resolve, reject) => {
       db.query(total, [], (err, result) => {
-        if (err) reject(err);
-        else resolve(result);
-      });
-    });
+        if (err) reject(err)
+        else resolve(result)
+      })
+    })
 
-    Promise.all([getSql, totalSql])
+    const totalEksistingSql = new Promise((resolve, reject) => {
+      db.query(totalEksisting, [], (err, result) => {
+        if (err) reject(err)
+        else resolve(result)
+      })
+    })
+
+    const totalBebasSql = new Promise((resolve, reject) => {
+      db.query(totalBebas, [], (err, result) => {
+        if (err) reject(err)
+        else resolve(result)
+      })
+    })
+
+    Promise.all([getSql, totalSql, totalEksistingSql, totalBebasSql])
       .then(result => {
-        const page = offset / limit + 1;
-        const total = result[1].map(i => i.total)[0];
-        const hasNext = total - page * limit > 0 ? true : false;
+        const page = offset / limit + 1
+        const total = result[1].map(i => i.total)[0]
+        const hasNext = total - page * limit > 0 ? true : false
         const pagination = {
           page,
           hasNext,
           total
-        };
+        }
+        const totalEksisting = result[2].map(i => i.total)[0]
+        const totalBebas = result[3].map(i => i.total)[0]
+        const totalByMerchantStatus = {
+          eksisting: totalEksisting,
+          bebas: totalBebas
+        }
         if (_csv) {
-          res.set("Content-Type", "text/csv");
+          res.set("Content-Type", "text/csv")
           res.setHeader(
             "Content-Disposition",
             'attachment; filename="' + "tempat-usaha-" + Date.now() + '.csv"'
-          );
-          stringify(result[0], { header: true }).pipe(res);
+          )
+          stringify(result[0], { header: true }).pipe(res)
         } else if (_xls) {
-          exportToExcel(res, result[0]);
+          exportToExcel(res, result[0])
         } else {
-          sendResponse(res, 200, { result: result[0], pagination });
+          sendResponse(res, 200, {
+            result: result[0],
+            pagination,
+            total: totalByMerchantStatus
+          })
         }
       })
       .catch(err => {
         sendResponse(res, 500, {
           response: "error_when_get_all_merchants",
           err
-        });
-      });
+        })
+      })
   },
 
   getMerchantById: (req, res) => {
-    const { id } = req.params;
-    const { summary } = req.query;
+    const { id } = req.params
+    const { summary } = req.query
 
     let sql = `
     SELECT *, T.name,
@@ -206,7 +232,7 @@ module.exports = {
            FROM merchants M
            JOIN tenants T
            ON M.tenant_id = T.no_ktp
-    WHERE M.merchant_no = ?;`;
+    WHERE M.merchant_no = ?;`
 
     if (summary === "true") {
       sql = `
@@ -218,21 +244,21 @@ module.exports = {
             FROM merchants M
             JOIN tenants T
             ON M.tenant_id = T.no_ktp
-      WHERE M.merchant_no = ?;`;
+      WHERE M.merchant_no = ?;`
     }
 
     db.query(sql, [id], (err, result) => {
       if (err) {
-        sendResponse(res, 500, { response: "error_when_get_merchants", err });
+        sendResponse(res, 500, { response: "error_when_get_merchants", err })
       } else {
-        sendResponse(res, 200, result);
+        sendResponse(res, 200, result)
       }
-    });
+    })
   },
 
   updateMerchantId: (req, res) => {
-    const { id } = req.params;
-    const user_id = req.user[0].id;
+    const { id } = req.params
+    const user_id = req.user[0].id
     const {
       tenant_id,
       merchant_status,
@@ -242,13 +268,13 @@ module.exports = {
       merchant_space,
       price_per_meter,
       total_price
-    } = req.body;
+    } = req.body
 
-    let _tenantId = merchant_status === "bebas" ? "0000000000000000" : tenant_id;
-    let attachment_1 = req.files["attachment_1"];
-    let attachment_2 = req.files["attachment_2"];
-    let _attachment_1;
-    let _attachment_2;
+    let _tenantId = merchant_status === "bebas" ? "0000000000000000" : tenant_id
+    let attachment_1 = req.files["attachment_1"]
+    let attachment_2 = req.files["attachment_2"]
+    let _attachment_1
+    let _attachment_2
 
     let data = [
       _tenantId,
@@ -259,7 +285,7 @@ module.exports = {
       merchant_space,
       price_per_meter,
       total_price
-    ];
+    ]
 
     // ============== SQL Script
     let sql = `
@@ -272,54 +298,54 @@ module.exports = {
           merchant_space = ?,
           price_per_meter = ?,
           total_price = ?
-    `;
-    const getOld = `SELECT * FROM merchants WHERE merchant_no = ?;`;
+    `
+    const getOld = `SELECT * FROM merchants WHERE merchant_no = ?;`
     // ==========================
 
     if (attachment_1 && attachment_2) {
-      uploadFile(attachment_1[0], "attachment_1", id);
-      uploadFile(attachment_2[0], "attachment_2", id);
+      uploadFile(attachment_1[0], "attachment_1", id)
+      uploadFile(attachment_2[0], "attachment_2", id)
 
-      _attachment_1 = `${AWS_LINK}${id}-attachment_1.jpg`;
-      _attachment_2 = `${AWS_LINK}${id}-attachment_2.jpg`;
+      _attachment_1 = `${AWS_LINK}${id}-attachment_1.jpg`
+      _attachment_2 = `${AWS_LINK}${id}-attachment_2.jpg`
 
-      sql += `, attachment_1 = ? `;
-      sql += `, attachment_2 = ? `;
+      sql += `, attachment_1 = ? `
+      sql += `, attachment_2 = ? `
 
-      data.push(_attachment_1);
-      data.push(_attachment_2);
+      data.push(_attachment_1)
+      data.push(_attachment_2)
     } else if (attachment_1) {
-      uploadFile(attachment_1[0], "attachment_1", id);
-      _attachment_1 = `${AWS_LINK}${id}-attachment_1.jpg`;
-      sql += `, attachment_1 = ?`;
-      data.push(_attachment_1);
+      uploadFile(attachment_1[0], "attachment_1", id)
+      _attachment_1 = `${AWS_LINK}${id}-attachment_1.jpg`
+      sql += `, attachment_1 = ?`
+      data.push(_attachment_1)
     } else if (attachment_2) {
-      uploadFile(attachment_2[0], "attachment_2", id);
-      _attachment_2 = `${AWS_LINK}${id}-attachment_2.jpg`;
-      sql += `, attachment_2 = ?`;
-      data.push(_attachment_2);
+      uploadFile(attachment_2[0], "attachment_2", id)
+      _attachment_2 = `${AWS_LINK}${id}-attachment_2.jpg`
+      sql += `, attachment_2 = ?`
+      data.push(_attachment_2)
     }
 
-    data.push(id);
-    sql += `, updated_at = DATE(NOW()) WHERE merchant_no = ?`;
+    data.push(id)
+    sql += `, updated_at = DATE(NOW()) WHERE merchant_no = ?`
 
     const getOldSql = new Promise((resolve, reject) => {
       db.query(getOld, [id], (err, result) => {
-        if (err) reject(err);
-        else resolve(result);
-      });
-    });
+        if (err) reject(err)
+        else resolve(result)
+      })
+    })
 
     const updateNewSql = new Promise((resolve, reject) => {
       db.query(sql, data, (err, result) => {
-        if (err) reject(err);
-        else resolve(result);
-      });
-    });
+        if (err) reject(err)
+        else resolve(result)
+      })
+    })
 
     Promise.all([getOldSql, updateNewSql])
       .then(async result => {
-        const oldData = result[0][0];
+        const oldData = result[0][0]
 
         let newData = {
           tenant_id: _tenantId,
@@ -330,40 +356,40 @@ module.exports = {
           merchant_space,
           price_per_meter,
           total_price
-        };
+        }
 
         if (attachment_1) {
-          newData["attachment_1"] = attachment_1;
+          newData["attachment_1"] = attachment_1
         }
 
         if (attachment_2) {
-          newData["attachment_2"] = attachment_2;
+          newData["attachment_2"] = attachment_2
         }
 
-        await merchantLogs(user_id, id, newData, oldData);
-        await sendResponse(res, 200, { result: result[1] });
+        await merchantLogs(user_id, id, newData, oldData)
+        await sendResponse(res, 200, { result: result[1] })
       })
       .catch(err => {
         sendResponse(res, 500, {
           response: "error_when_update_tenant",
           err
-        });
-      });
+        })
+      })
   },
 
   deleteMerchantById: (req, res) => {
-    const { id } = req.params;
-    const sql = `DELETE FROM merchants WHERE merchant_no = ? ;`;
+    const { id } = req.params
+    const sql = `DELETE FROM merchants WHERE merchant_no = ? ;`
 
     db.query(sql, [id], (err, result) => {
       if (err) {
         sendResponse(res, 500, {
           response: "error_when_delete_merchants",
           err
-        });
+        })
       } else {
-        sendResponse(res, 200, result);
+        sendResponse(res, 200, result)
       }
-    });
+    })
   }
-};
+}
